bc5fd455a663c37fdb40cac9f8f9ccfd9dcbf59f
HSteffensen <henry@steffensenfamily.com>
2022-01-02 15:41:40 +0100

    tailwind and settings

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,14 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        'auto': 'repeat(auto-fit, minmax(8rem, auto))'
+      },
+      gridTemplateRows: {
+        'auto': 'repeat(auto-fit, 1fr)'
+      },
+    },
   },
   variants: {
     extend: {},
